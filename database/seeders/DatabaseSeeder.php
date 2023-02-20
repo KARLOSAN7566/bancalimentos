41d21075eff472c63bc259abb3d8147a47001b26
KARLOSAN7566 <kers_1209@hotmail.com>
2023-02-20 09:41:15 -0500

    Migracios Seeders 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\DataTypesTableSeeder;
+use Database\Seeders\VoyagerDatabaseSeeder;
+use Database\Seeders\PermissionsTableSeeder;
+use Database\Seeders\PermissionRoleTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,20 +25,28 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(CitiesTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
         $this->call(CustomersTableSeeder::class);
+        $this->call(CountriesTableSeeder::class);
+        $this->call(DataRowsTableSeeder::class);
+        $this->call(DataTypesTableSeeder::class);
+        $this->call(FailedJobsTableSeeder::class);
         $this->call(MaterialsTableSeeder::class);
-        $this->call(MenusTableSeeder::class);
         $this->call(MenuItemsTableSeeder::class);
+        $this->call(MenusTableSeeder::class);
         $this->call(MigrationsTableSeeder::class);
         $this->call(PartnersTableSeeder::class);
+        $this->call(PasswordResetsTableSeeder::class);
+        $this->call(PermissionRoleTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(PersonalAccessTokensTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(SettingsTableSeeder::class);
         $this->call(SitesTableSeeder::class);
         $this->call(StatesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(FailedJobsTableSeeder::class);
-        $this->call(PasswordResetsTableSeeder::class);
-        $this->call(PersonalAccessTokensTableSeeder::class);
+        $this->call(TraslationsTableSeeder::class);
         $this->call(UserRolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(VoyagerDatabaseSeeder::class);
     }
 }
